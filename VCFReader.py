4f635424e5be064f96173fd5db6cbce9aba019a8
Chase <coderchase@outlook.com>
2015-10-14 09:28:29 +0800

    Add VCFReader.py

---
+++ VCFReader.py
@@ -0,0 +1,29 @@
+import vcf
+
+class VCFReader:
+    def __init__(self, vcf_file):
+        try:
+            vfile = open(vcf_file, 'r')
+            self.vcf_reader = vcf.Reader(vfile)
+        except IOException:
+            print 'This VCF file does not exist: ' + vcf_file
+    
+    # Given a region, check the number of sites which are heterozygous calls
+    def getSNPInfo(self, sample_id, intervals_list):
+        snp_info = list()
+        if self.vcf_reader:
+            for in_list in intervals_list:
+                for interval in in_list:
+                    # The start and end coordinates are in the zero-based, half-open coordinate system
+                    records = self.vcf_reader.fetch(interval._chr, interval._bp1, interval._bp2 + 1) 
+                    if records:
+                        het_num = 0
+                        for record in records:
+                            genotype = record.genotype(sample_id)
+                            if genotype and genotype.is_het:
+                                het_num += 1    
+                        snp_info.append(het_num)
+                    else:
+                        snp_info.append(0)
+                        
+        return snp_info
